55e9a8a1d1e61ae9d6cc4465c69ef3a62526c0ac
Kavuti <christian.cavuti@gmail.com>
2020-10-30 00:20:41 +0100

    Added other noob code

---
+++ cards/deck_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spades as first card, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected Four of Clubs as last card, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	filename := "_decktesting"
+	os.Remove(filename)
+	d := newDeck()
+	d.saveToFile(filename)
+	loadedDeck := newDeckFromFile(filename)
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, but got %v", len(loadedDeck))
+	}
+
+	os.Remove(filename)
+}
